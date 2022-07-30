90cb6eddfb9106f6f5c5716b65873db350578f8b
Matthias Weaser <mw3597@columbia.edu>
2022-07-30 07:49:02 -0700

    Time: 40 ms (84.21%), Space: 14.1 MB (82.02%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        if not nums:
+            return
+        
+        subsets = []
+        subsets.append([])
+        
+        for currentNumber in nums:
+            
+            n = len(subsets)
+            
+            for i in range(n):
+                set1 = list(subsets[i])
+                set1.append(currentNumber)
+                subsets.append(set1)
+                
+        return subsets
+        
