4bc4cb75e5f8a937a751edc24e363171c0ae4da7
Matthias Weaser <mw3597@columbia.edu>
2022-08-01 14:06:43 -0700

    Time: 563 ms (32.68%), Space: 22 MB (19.62%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,27 +4,28 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         visited = set()
-        count = 0
+        count = 0 
         
         def bfs(r,c):
             queue = deque()
             queue.append((r,c))
             visited.add((r,c))
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            directions = [[1,0],[-1,0],[0,1],[0,-1]]    
             
             while queue:
                 row, col = queue.popleft()
+                
                 for dr, dc in directions:
-                    r = row + dr
-                    c = col + dc
-                    
+                    r, c = dr + row, dc + col
                     if r in range(rows) and c in range(cols) and (r,c) not in visited and grid[r][c] == "1":
                         queue.append((r,c))
                         visited.add((r,c))
-            
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visited:
                     bfs(r,c)
                     count += 1
         return count
+                
+        
