d72b99804a26cd5b8a8d2f455a81fdab02b5ea76
Matthias Weaser <mw3597@columbia.edu>
2022-10-17 13:55:39 -0400

    Time: 40 ms (86.57%), Space: 13.8 MB (60.67%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        total = 0
+
+        while n >= 5:
+            n = n // 5 
+            total += n
+                    
+        return total
+        
