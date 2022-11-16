c85ecedacd30f36a8f8b186e15f445112be5a0a0
Matthias Weaser <mw3597@columbia.edu>
2022-11-16 11:30:46 -0500

    Time: 61 ms (67.02%), Space: 13.9 MB (75.61%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,31 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        res = []
+        
+        top = 0
+        bottom = len(matrix)
+        left = 0
+        right = len(matrix[0])
+        
+        while left < right:
+            curr = []
+            for i in range(top, bottom):
+                curr.append(matrix[i][left])
+            # print("curr:", curr)
+            left += 1
+            curr.reverse()
+            res.append(curr)
+            print("res is now:", res)
+            
+        for i in range(len(matrix)):
+            matrix[i] = res[i]
+
+
+            
+                
+            
+        
