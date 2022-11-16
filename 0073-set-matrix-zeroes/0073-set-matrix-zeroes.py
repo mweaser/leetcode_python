d95ced67834b14f2d4dad807e9607ca4969b3913
Matthias Weaser <mw3597@columbia.edu>
2022-11-15 23:12:27 -0500

    Time: 979 ms (5.06%), Space: 32.9 MB (16.64%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -22,21 +22,6 @@ class Solution:
                 if j in cols:
                     matrix[i][j] = 0
                 
+         
                     
-        print(rows)
-        print(cols)
         
-        print(np.matrix(matrix))
-            
-                        
-                    
-                
-                
-                
-                
-# #         -1,0 1,0 0,-1 0,1
-
-# matrix[i-1][j] = 0 and (i-1 in range(len(matrix)) and j in range(len(matrix[i])))
-                    # matrix[i+1][j] = 0 and (i+1 in range(len(matrix)) and j in range(len(matrix[i])))
-                    # matrix[i][j-1] = 0 and (i in range(len(matrix)) and j in range(len(matrix[i])))
-                    # matrix[i][j+1] = 0 and (i in range(len(matrix)) and j in range(len(matrix[i])))
