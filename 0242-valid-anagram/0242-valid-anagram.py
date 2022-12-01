c7dd2991fcdb2c77ba56d9c8ae5451d96ced5b8e
Matthias Weaser <mw3597@columbia.edu>
2022-12-01 12:48:43 -0500

    Time: 113 ms (40.17%), Space: 15.2 MB (11.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        s = list(s)
-        t = list(t)
+        s, t = (list(s)), (list(t))
         
         s.sort()
         t.sort()
-        
-        print(s)
-        print(t)
-        
         return s == t
-        
