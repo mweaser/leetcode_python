0a5d6e4cad3790223eb18ac5e9475728cd283555
Matthias Weaser <mw3597@columbia.edu>
2022-12-02 16:48:50 -0500

    Time: 110 ms (89.15%), Space: 18.6 MB (35.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,11 @@
 class Solution:
     def groupAnagrams(self, ele: List[str]) -> List[List[str]]:
-        
-        words = []
-        for e in ele:
-            words.append(list(e))
                     
         seen = {}
         res = []
         
-        for w in words:
-            sorted_w = "".join(sorted(w))
+        for w in ele:
+            sorted_w = "".join(sorted(list(w)))
             if sorted_w not in seen:
                 seen[sorted_w] = []
             seen[sorted_w].append("".join(w))
