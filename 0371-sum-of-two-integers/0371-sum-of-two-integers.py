efa5e99d6d82feed6a285c19a0e84e3f88608203
Matthias Weaser <mw3597@columbia.edu>
2022-12-05 10:39:41 -0500

    Time: 39 ms (79.66%), Space: 13.8 MB (64.23%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        
+        l = []
+        l.extend([a])
+        l.extend([b])
+        
+        return sum(l)
+        
+        
+        
